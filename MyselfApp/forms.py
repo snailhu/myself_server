ae945115c1099c5ca8aee01d2b5a100ecc3009ec
snailhu <snailhu@yahoo.com>
2014-01-24 15:45:46 +0800

    [SD] push code .

--- MyselfApp/forms.py
+++ MyselfApp/forms.py
@@ -1,15 +1,15 @@
-__author__ = 'Administrator'
-
-
-from django import forms
-from captcha.fields import CaptchaField
-
-#class CaptchaTestForm(forms.Form):
-#    #myfield = AnyOtherField()
-#    captcha = CaptchaField()
-
-#captcha=CaptchaField()
-from django import forms
-class UploadFileForm(forms.Form):
-  title = forms.CharField(max_length=50)
-  file = forms.FileField()
+#__author__ = 'Administrator'
+#
+#
+#from django import forms
+#from captcha.fields import CaptchaField
+#
+##class CaptchaTestForm(forms.Form):
+##    #myfield = AnyOtherField()
+##    captcha = CaptchaField()
+#
+##captcha=CaptchaField()
+#from django import forms
+#class UploadFileForm(forms.Form):
+#  title = forms.CharField(max_length=50)
+#  file = forms.FileField()
